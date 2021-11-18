1d7c43c3d77b5136c86d0f1a5d5b82317a8cd831
David <dave_martins@hotmail.com>
2021-11-18 08:59:36 -0500

    Update/Arreglos Introducción

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -1,2 +1,25 @@
 //02-arreglos.js
-const arreglo = [6, 7, 8, 9, 10]
+let arreglo = [6, 7, 8, 9, 10];
+arreglo = 1;
+arreglo = true;
+arreglo = undefined;
+arreglo = null;
+arreglo = {};
+arreglo = [true, 1, 1.1, "David", "Morales", undefined, null, {}, [1, 2]];
+arreglo = [6, 7, 8, 9, 10];
+
+//for of
+for (let numero of arreglo) {
+    console.log("numero", numero);
+}
+
+//for in
+for (indice in arreglo){
+    arreglo[indice];
+    console.log("indice", indice);
+}
+
+let objetoPrueba = {a:"1", b: "2", c: "3"};
+for (llave in objetoPrueba){
+    console.log("llave", llave);
+}
