4bb2c1ddecea6789808412f9c917b7666e6a74b1
David <dave_martins@hotmail.com>
2021-12-02 08:58:40 -0500

    Update/ Async y Await

---
+++ 01-javascript/11-inquirer.js
@@ -0,0 +1,27 @@
+//11-inquirer.js
+
+const inquirer = require('inquirer');
+
+// Necesitamos instalar inquirer
+async function main() {
+    try{
+        const respuesta = await inquirer
+            .prompt([
+                {
+                    type:'input',
+                    name: 'apellido',
+                    message: 'Ingresa tu Nombre: '
+                },
+                {
+                    type:'input',
+                    name: 'edad',
+                    message: 'Ingresa tu Edad: '
+                }
+            ]);
+        console.log('Respuesta', respuesta);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main();
